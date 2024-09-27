b31f023fa25c05e133dd73c08d7d89cb63f84bab
Tonny Bett <betttonny26@gmail.com>
2024-09-27 23:02:45 +0300

    added booking and changed display in app in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './components/Landing'
-
+import Booking from './components/Booking'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-    <Landing/>
+    <div>
+      <Landing/>
+    </div>
+    <div>
+      <section>
+        <Booking/>
+      </section>
+    </div>
    </div>
   )
 }
