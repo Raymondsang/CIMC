536f16d0a0337e978dfda7e2204c8b692080beb4
Tonny Bett <betttonny26@gmail.com>
2025-01-05 11:11:12 +0300

    complete env setup with no errors

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,45 +1,18 @@
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-// import { getDatabase } from "firebase/database";
-
-// const firebaseConfig = {
-    // apiKey: "AIzaSyDdyA17YHl-ko-LM7F8wKQe5OOjlou7Ir4",
-    // authDomain: "cimc-4b6b4.firebaseapp.com",
-    // projectId: "cimc-4b6b4",
-    // storageBucket: "cimc-4b6b4.appspot.com",
-    // messagingSenderId: "963789662119",
-    // appId: "1:963789662119:web:18b5d34b9dd42389b67882",
-    // measurementId: "G-0JM9BD50CJ",
-// };
-
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app)
-
-// export { db, app };
-// const storage = getStorage(app);
-// const auth = getAuth(app);
-// const database = getDatabase(app);
-
-// export { auth,storage, database}; 
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdyA17YHl-ko-LM7F8wKQe5OOjlou7Ir4",
-    authDomain: "cimc-4b6b4.firebaseapp.com",
-    projectId: "cimc-4b6b4",
-    storageBucket: "cimc-4b6b4.appspot.com",
-    messagingSenderId: "963789662119",
-    appId: "1:963789662119:web:18b5d34b9dd42389b67882",
-    measurementId: "G-0JM9BD50CJ",
- 
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
